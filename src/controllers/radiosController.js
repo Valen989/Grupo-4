81b83515363cbc291ff22233fb1e2424125e5c61
Valentino Parma <parmavalentinop@gmail.com>
2024-11-05 10:38:34 -0300

    presentacion 5-11

--- src/controllers/radiosController.js
+++ src/controllers/radiosController.js
@@ -1,3 +1,8 @@
+const path = require('path');
+const { getData, storeData } = require("../data");
+const radios = getData("radios.json");
+
+
 module.exports = {
     list : (req,res) => {
         return res.render('radios/list')
@@ -7,7 +12,33 @@ module.exports = {
 
     },
     create : (req,res) => {
-        return res.send(req.body)
+        create: (req, res) => {
+            const errors = validationResult(req);
+        
+            if(errors.isEmpty()){
+              const { name, city, province, frequency, admin } = req.body;
+            
+              //guardar datos
+              const newProduct = {
+                id: +radios[radios.length - 1].id + 1,
+                name: name.trim(),
+                city: city.trim(),
+                province: province.trim(),
+                frequency: +frequency,
+                admin: +admin,
+                image: req.file ? req.file.filename : "http://dummyimage.com/200x300.png/cc0000/ffffff",
+              };
+          
+              radios.push(newRadios);
+          
+              storeData(radios,'radios.json')
+          
+              //respuesta al cliente
+              return res.redirect(`/radios/${newRadio.id}`)
+            }
+            return res.send(req.body)
+          },
+        
 
     },
     edit : (req,res) => {
@@ -23,3 +54,7 @@ module.exports = {
 
     }
 }
+
+
+
+
