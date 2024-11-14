9dc3d6114134f77caba744456975377c80f568b3
nahuel <nahueltello01@gmail.com>
2024-11-14 10:57:49 -0300

    ultima presentacion

--- src/routes/streams.routes.js
+++ src/routes/streams.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { index, list, add, create, edit, update, destroy, detail } = require('../controllers/streamsController');
+const { index, list, add, create, edit, update, destroy, detail, } = require('../controllers/streamsController');
 const checkUserLogin = require('../middlewares/checkUserLogin');
 
 //streams
@@ -15,6 +15,7 @@ router
     .get('/edit/:stream_id',checkUserLogin, edit)
     .put('/update/:stream_id',update)
     .delete('/destroy/:stream_id',destroy)
+    
 
 
 
